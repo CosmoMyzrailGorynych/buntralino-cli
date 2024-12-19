0638358099a6b17bcb303032390ae8b96aa4c0b1
Cosmo Myzrail Gorynych <admin@nersta.ru>
2024-12-19 22:26:58 +1200

    :zap: Update the default template with a visual bun connection check and a neat glow animation

--- templates/new/src/neutralino/index.ts
+++ templates/new/src/neutralino/index.ts
@@ -3,13 +3,26 @@ import * as buntralino from 'buntralino-client';
 // Sample Bun interaction
 (async () => {
     await buntralino.ready;
-    const response = await buntralino.run('sayHello', {
-        message: 'Hello, Buntralino!'
-    });
-    if (typeof response === 'string') {
+    const TIMEOUT = {};
+    const response = await Promise.race([
+        buntralino.run('sayHello', {
+            message: 'Hello, Buntralino!'
+        }),
+        new Promise(resolve => setTimeout(() => resolve(TIMEOUT), 5000))
+    ]);
+    const checkDiv = document.getElementById('theBunCheck')!;
+    if (response === TIMEOUT) {
+        console.error('No connection in 5 seconds');
+        checkDiv.innerHTML = '⛔ ' + response;
+        checkDiv.classList.add('error');
+    } else if (typeof response === 'string') {
         console.log(response);
+        checkDiv.innerHTML = '✅ ' + response;
+        checkDiv.classList.add('success');
     } else {
         console.error('Got unknown response from Bun:', response);
+        checkDiv.innerHTML = 'Got unknown response from Bun 🤷 Did you change the method on Bun side?';
+        checkDiv.classList.add('warning');
     }
 })();
 
