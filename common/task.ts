1ca3a5e9326ef95e2649290f8bc6ef6e6d194070
Cosmo Myzrail Gorynych <admin@nersta.ru>
2024-12-28 06:14:38 +1200

    :zap: Explicitly mark failed tasks in command output with an X

--- common/task.ts
+++ common/task.ts
@@ -5,7 +5,12 @@ export default async (opts: {
     finish?: string
 }, promise: Promise<unknown>) => {
     const spinner = new Spinner().start(opts.text);
-    const val = await promise;
-    spinner.succeed(opts.finish ?? opts.text);
-    return val;
+    try {
+        const val = await promise;
+        spinner.succeed(opts.finish ?? opts.text);
+        return val;
+    } catch (error) {
+        spinner.failed(opts.text);
+        throw error;
+    }
 };
